130bad3f4b8dce259fb9fcc7032a403d0da569a1
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2026-01-22 10:36:06 +0100

    wip: kyc process screen conversion

--- src/screens/main/verification/kycProcess/index.tsx
+++ src/screens/main/verification/kycProcess/index.tsx
@@ -1,7 +1,29 @@
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+
 const KYCProcess = () => {
-    return (
-        <div>KYC Process</div>
-    )
-}
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set());
+
+  const isStepSkipped = (step: number) => {
+    return skipped.has(step);
+  };
+
+  return (
+    <Stack>
+      <Box border="1px solid #EFEFEF">
+        
+      </Box>
+    </Stack>
+  );
+};
 
 export default KYCProcess;
+
+const steps = [
+  "Select Country",
+  "Verify Phone Number",
+  "Verify Identity (NIN for Nigerians & government issued ID for other countries)",
+  "Add Bank Account",
+];
